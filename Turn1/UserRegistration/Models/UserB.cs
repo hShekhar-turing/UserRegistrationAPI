260256a64a3b0d79a2299ea74d6c6d44454e0e1f
Himanshu Shukla <Himanshu.Shukla@ClickMinds.com>
2024-11-21 01:08:13 +0530

    Changes related to React and improvement in Annotations.

--- Turn1/UserRegistration/Models/UserB.cs
+++ Turn1/UserRegistration/Models/UserB.cs
@@ -8,40 +8,49 @@ namespace UserRegistrationAPI.Models
     /// </summary>
     public class UserB
     {
-        /// <summary>
-        /// Gets or sets the user's email address.
-        /// </summary>
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
-        /// <summary>
-        /// Gets or sets the user's password.
-        /// </summary>
         [Required(ErrorMessage = "Password is required")]
         [StringLength(100, ErrorMessage = "Password must be between 6 and 100 characters", MinimumLength = 6)]
         public string Password { get; set; }
 
-        /// <summary>
-        /// Gets or sets the user's phone number.
-        /// </summary>
-        [Required(ErrorMessage = "Phone number is required")]
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(50, ErrorMessage = "First Name must be between 2 and 50 characters", MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(50, ErrorMessage = "Last Name must be between 2 and 50 characters", MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
         [Phone(ErrorMessage = "Invalid phone number")]
         public string PhoneNumber { get; set; }
 
-        /// <summary>
-        /// Gets or sets the user's first name.
-        /// </summary>
-        [Required(ErrorMessage = "First name is required")]
-        [StringLength(50, ErrorMessage = "First name must be between 2 and 50 characters", MinimumLength = 2)]
-        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Street Address is required")]
+        [StringLength(100, ErrorMessage = "Street Address must be between 5 and 100 characters", MinimumLength = 5)]
+        public string StreetAddress { get; set; }
 
-        /// <summary>
-        /// Gets or sets the user's last name.
-        /// </summary>
-        [Required(ErrorMessage = "Last name is required")]
-        [StringLength(50, ErrorMessage = "Last name must be between 2 and 50 characters", MinimumLength = 2)]
-        public string LastName { get; set; }
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(50, ErrorMessage = "City must be between 2 and 50 characters", MinimumLength = 2)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "State is required")]
+        [StringLength(50, ErrorMessage = "State must be between 2 and 50 characters", MinimumLength = 2)]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Zip Code is required")]
+        [StringLength(10, ErrorMessage = "Zip Code must be between 5 and 10 characters", MinimumLength = 5)]
+        public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(50, ErrorMessage = "Country must be between 2 and 50 characters", MinimumLength = 2)]
+        public string Country { get; set; }
     }
 
     /// <summary>
@@ -49,40 +58,48 @@ namespace UserRegistrationAPI.Models
     /// </summary>
     public class UserRegistrationModel
     {
-        /// <summary>
-        /// Gets or sets the user's first name.
-        /// </summary>
-        [Required(ErrorMessage = "First name is required")]
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 50 characters")]
-        public string FirstName { get; set; }
-
-        /// <summary>
-        /// Gets or sets the user's last name.
-        /// </summary>
-        [Required(ErrorMessage = "Last name is required")]
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 50 characters")]
-        public string LastName { get; set; }
-
-        /// <summary>
-        /// Gets or sets the user's email address.
-        /// </summary>
-        [Required(ErrorMessage = "Email address is required")]
+        [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
-        /// <summary>
-        /// Gets or sets the user's password.
-        /// </summary>
         [Required(ErrorMessage = "Password is required")]
-        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character")]
+        [StringLength(100, ErrorMessage = "Password must be between 6 and 100 characters", MinimumLength = 6)]
         public string Password { get; set; }
 
-        /// <summary>
-        /// Gets or sets the user's phone number.
-        /// </summary>
-        [Required(ErrorMessage = "Phone number is required")]
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(50, ErrorMessage = "First Name must be between 2 and 50 characters", MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(50, ErrorMessage = "Last Name must be between 2 and 50 characters", MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
         [Phone(ErrorMessage = "Invalid phone number")]
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Physical Address is required")]
+        [StringLength(100, ErrorMessage = "Physical Address must be between 10 and 100 characters", MinimumLength = 10)]
+        public string PhysicalAddress { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(50, ErrorMessage = "City must be between 2 and 50 characters", MinimumLength = 2)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "State is required")]
+        [StringLength(50, ErrorMessage = "State must be between 2 and 50 characters", MinimumLength = 2)]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Zip Code is required")]
+        [StringLength(10, ErrorMessage = "Zip Code must be between 5 and 10 characters", MinimumLength = 5)]
+        public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(50, ErrorMessage = "Country must be between 2 and 50 characters", MinimumLength = 2)]
+        public string Country { get; set; }
     }
 }
